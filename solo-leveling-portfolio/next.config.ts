530125611a0c26c54c8ff1b9ad5c4cf959a2290b
DanielBarillasM <146965525+DanielBarillasM@users.noreply.github.com>
2025-06-01 12:06:54 -0600

    Arreglando errores de dependencias y package.json

--- solo-leveling-portfolio/next.config.ts
+++ solo-leveling-portfolio/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",  
   basePath: "/Porfafolio_Web_22193",
   assetPrefix: "/Porfafolio_Web_22193/",
   images: {
