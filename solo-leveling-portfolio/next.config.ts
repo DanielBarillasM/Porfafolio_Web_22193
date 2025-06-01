1229318ea1bbdba425073ec1935c2cbd07bb7d37
DanielBarillasM <146965525+DanielBarillasM@users.noreply.github.com>
2025-06-01 12:00:32 -0600

    Arreglando errores para subir a github pages

--- solo-leveling-portfolio/next.config.ts
+++ solo-leveling-portfolio/next.config.ts
@@ -1,12 +1,10 @@
-// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Dependiendo de tu configuración previa, si ya tenías images:{…}, agrégalo aquí también.
   basePath: "/Porfafolio_Web_22193",
   assetPrefix: "/Porfafolio_Web_22193/",
   images: {
-    domains: ["picsum.photos"], // si ya lo tenías, mantenlo
+    domains: ["picsum.photos"],
   },
 };
 
